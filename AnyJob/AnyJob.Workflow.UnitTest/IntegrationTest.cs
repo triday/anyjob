319ae37e56b25f3dbd8f2e3bc33609aa868ce4ca
杨鹏博 <obpt@sina.com>
2019-08-01 23:35:47 +0800

    完成工作流集成

--- AnyJob/AnyJob.Workflow.UnitTest/IntegrationTest.cs
+++ AnyJob/AnyJob.Workflow.UnitTest/IntegrationTest.cs
@@ -7,17 +7,14 @@ namespace AnyJob.Workflow
     [TestClass]
     public class IntegrationTest : JobTestBase
     {
-        //[TestMethod]
+        [TestMethod]
         public void TestEmpty()
         {
-            var inputs = new Dictionary<string, object>()
-            {
-                { "num1" , 100 },
-                { "num2" , 200 }
-            };
-            var job = JobEngine.Start("nodepack.add", inputs);
+            var inputs = new Dictionary<string, object>();
+            var job = JobEngine.Start("workflow.empty", inputs);
             var result = job.Task.Result;
             Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual("empty", result.Result);
         }
     }
 }
--- AnyJob/AnyJob.Workflow/Models/WorkflowInfo.cs
+++ AnyJob/AnyJob.Workflow/Models/WorkflowInfo.cs
@@ -6,16 +6,29 @@ namespace AnyJob.Workflow.Models
 {
     public class WorkflowInfo
     {
+        /// <summary>
+        /// 获取或设置工作流文件的版本号
+        /// </summary>
         public string Version { get; set; }
-
+        /// <summary>
+        /// 获取或设置工作流预处理分组信息
+        /// </summary>
         public GroupInfo Setup { get; set; }
-
+        /// <summary>
+        /// 获取或设置工作流执行分组信息
+        /// </summary>
         public GroupInfo Body { get; set; }
-
+        /// <summary>
+        /// 获取或设置工作流后处理分组信息
+        /// </summary>
         public GroupInfo TearDown { get; set; }
-
+        /// <summary>
+        /// 获取或设置工作流初始的全局变量信息
+        /// </summary>
         public Dictionary<string, object> GlobalVars { get; set; }
-
-        public Object Output { get; set; }
+        /// <summary>
+        /// 获取或设置工作流的输出信息
+        /// </summary>
+        public object Output { get; set; }
     }
 }
