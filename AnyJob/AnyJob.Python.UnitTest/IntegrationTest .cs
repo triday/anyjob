87dcf9f5d74ad50bc85f12ac966d6bded2359ee4
杨鹏博 <obpt@sina.com>
2019-08-09 08:12:17 +0800

    python返回结构化数据和错误，支持大数据传输

---
+++ AnyJob/AnyJob.Python.UnitTest/IntegrationTest .cs
@@ -0,0 +1,44 @@
+﻿using AnyJob;
+using AnyJob.UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyJob.Python
+{
+    [TestClass]
+    public class IntegrationTest : JobTestBase
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var inputs = new Dictionary<string, object>()
+            {
+                { "num1" , 100 },
+                { "num2" , 200 }
+            };
+            var job = JobEngine.Start("pythonpack.add", inputs);
+            var result = job.Task.Result;
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual(300L, result.Result);
+        }
+        [TestMethod]
+        public void TestLargeInputData()
+        {
+            var arg1 = string.Join("\n", Enumerable.Range(1, 1000000).Select(p => $"{p:d8}"));
+            var arg2 = string.Join("\n", Enumerable.Range(100000000, 1000000).Select(p => $"{p:d8}"));
+            var inputs = new Dictionary<string, object>()
+            {
+                { "a" ,arg1 },
+                { "b" ,arg2 }
+            };
+            var job = JobEngine.Start("pythonpack.concat", inputs);
+            var result = job.Task.Result;
+            Assert.IsTrue(result.IsSuccess);
+            var resultText = result.Result as string;
+            Assert.AreEqual(arg1.Length + arg2.Length, resultText.Length);
+        }
+    }
+}
--- AnyJob/AnyJob.Python.UnitTest/global/python/add.py
+++
@@ -1,2 +0,0 @@
-def run(num1=1,num2=3):
-    return num1 + num2
