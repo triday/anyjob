2e0d3cd05f77d4cce284fd6e2c362ed37cc803a8
杨鹏博 <obpt@sina.com>
2019-08-16 22:51:30 +0800

    添加java类型的动作

---
+++ AnyJob/AnyJob.Java/JavaAction.cs
@@ -0,0 +1,20 @@
+﻿using AnyJob.Process;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnyJob.Java
+{
+    public class JavaAction : TypedProcessAction
+    {
+        public JavaAction(JavaOption option)
+        {
+
+        }
+
+        protected override (string FileName, string Arguments, string StandardInput) OnGetCommands(IActionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
