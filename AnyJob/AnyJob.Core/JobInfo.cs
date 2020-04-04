57b63c54d5d33042aca6050641c27b55f0307791
ypb <obpt@sina.com>
2020-04-04 12:16:40 +0800

    add logic projects

---
+++ AnyJob/AnyJob.Core/JobInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnyJob
+{
+    public class JobInfo
+    {
+        public string Status { get; set; }
+
+
+    }
+
+}
