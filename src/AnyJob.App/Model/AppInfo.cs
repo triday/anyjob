0a4f3504fc2b13fc9032036c5f7f7147168e69cf
pengbo.yang <obpt@sina.com>
2020-07-07 23:25:57 +0800

    add support platforms field in app action

--- src/AnyJob.App/Model/AppInfo.cs
+++ src/AnyJob.App/Model/AppInfo.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace AnyJob.App.Model
 {
     public class AppInfo
     {
+        public string[] SupportPlatforms { get; set; } = new string[] { nameof(OSPlatform.Windows), nameof(OSPlatform.Linux), nameof(OSPlatform.OSX) };
         public string Command { get; set; }
         public Dictionary<string, string> Envs { get; set; }
     }
