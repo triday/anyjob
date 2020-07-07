26e141c05734a3a3952b9031e87821df8520b96e
pengbo.yang <obpt@sina.com>
2020-07-08 00:37:14 +0800

    improve projects files, remove empty lines

--- test/AnyJob.Engine.UnitTest/Properties/Resources.Designer.cs
+++ test/AnyJob.Engine.UnitTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AnyJob.Properties {
+namespace AnyJob.Engine.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AnyJob.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AnyJob.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AnyJob.Engine.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
