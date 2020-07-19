37849606e370ad982b5abf9bb42e6d4a6feefbec
ypb <obpt@sina.com>
2020-07-19 23:07:28 +0800

    support multi providers

--- src/AnyJob.Engine/Impl/DefaultActionNameResolveService.cs
+++ src/AnyJob.Engine/Impl/DefaultActionNameResolveService.cs
@@ -1,28 +1,46 @@
 ﻿using System.Text.RegularExpressions;
+using AnyJob.Config;
 
 namespace AnyJob.Impl
 {
     [YS.Knife.ServiceClass(Lifetime = Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton)]
     public class DefaultActionNameResolveService : IActionNameResolveService
     {
-        private static Regex ActionFullnameRegex = new Regex(@"^(?<pack>\w+(\.\w+)*)\.(?<name>\w+)(@(?<version>\d+(\.\d+){1,3}))?$", RegexOptions.Compiled);
+        readonly PackOption packOption;
+        private readonly IPackageProviderService packageProviderService;
+        public DefaultActionNameResolveService(PackOption packOption, IPackageProviderService packageProviderService)
+        {
+            this.packOption = packOption;
+            this.packageProviderService = packageProviderService;
+        }
+        private static Regex ActionFullnameRegex = new Regex(@"^((?<provider>\w+):)?(?<pack>\w+(\.\w+)*)\.(?<name>\w+)(@(?<version>\d+(\.\d+){1,3}))?$", RegexOptions.Compiled);
 
         public virtual IActionName ResolverName(string fullName)
         {
             var match = ActionFullnameRegex.Match(fullName);
             if (match.Success)
             {
+                var pack = match.Groups["pack"].Value;
+                var name = match.Groups["name"].Value;
+                var provider = match.Groups["provider"].Success ? match.Groups["provider"].Value : packOption.DefaultProviderName;
+                var version = match.Groups["version"].Success ? match.Groups["version"].Value : GetLatestPackVersion(provider, pack);
                 return new ActionName
                 {
-                    Pack = match.Groups["pack"].Value,
-                    Name = match.Groups["name"].Value,
-                    Version = match.Groups["version"].Value
+                    Provider = provider,
+                    Pack = pack,
+                    Name = name,
+                    Version = version
                 };
             }
             else
             {
                 throw Errors.InvalidActionName(fullName);
             }
+        }
+        public virtual string GetLatestPackVersion(string provider, string package)
+        {
+            // TODO cache the result
+            return packageProviderService.GetLatestPackageVersion(provider, package);
         }
     }
 }
---
+++ src/AnyJob.Engine/Impl/DefaultPackageProviderService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using AnyJob.Config;
+
+namespace AnyJob.Impl
+{
+    public class DefaultPackageProviderService : IPackageProviderService
+    {
+        readonly PackOption packOption;
+        readonly ISerializeService serializeService;
+        public DefaultPackageProviderService(PackOption packOption, ISerializeService serializeService)
+        {
+            this.packOption = packOption;
+        }
+        public List<PackageFileInfo> GetAllPackageFiles(string provider, string package, string version)
+        {
+            var baseUrl = GetServiceBaseUrl(provider);
+            using (var client = new HttpClient())
+            {
+                var url = new Uri(baseUrl);
+                var fullUrl = new Uri(new Uri(baseUrl), $"packages/{package}/{version}/files");
+                var content = client.GetStringAsync(fullUrl).GetAwaiter().GetResult();
+                return serializeService.Deserialize<List<PackageFileInfo>>(content);
+            }
+        }
+
+        public string GetLatestPackageVersion(string provider, string package)
+        {
+            var baseUrl = GetServiceBaseUrl(provider);
+            using (var client = new HttpClient())
+            {
+                var url = new Uri(baseUrl);
+                var fullUrl = new Uri(new Uri(baseUrl), $"packages/{package}/versions/latest");
+                var content = client.GetStringAsync(fullUrl).GetAwaiter().GetResult();
+                return serializeService.Deserialize<PackageVersionInfo>(content).Version;
+            }
+        }
+
+        private string GetServiceBaseUrl(string provider)
+        {
+            if (packOption.Providers != null && packOption.Providers.TryGetValue(provider, out string url))
+            {
+                return url;
+            }
+            else
+            {
+                throw new Exception($"Can not find package provider '{provider}'.");
+            }
+        }
+        class PackageVersionInfo
+        {
+            public string Version { get; set; }
+            public string Description { get; set; }
+            public List<string> Tags { get; set; }
+        }
+    }
+}
