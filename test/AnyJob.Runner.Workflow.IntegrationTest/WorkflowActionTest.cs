4421ba02eb50861c61778cdc549474f7b312d063
ypb <obpt@sina.com>
2020-07-28 00:11:37 +0800

    migrate workflow integration test

---
+++ test/AnyJob.Runner.Workflow.IntegrationTest/WorkflowActionTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AnyJob.Runner.Workflow.IntegrationTest
+{
+    [TestClass]
+    public class WorkflowActionTest
+    {
+        [TestMethod]
+        public void ShouldSuccessWhenRunEmptyWorkflow()
+        {
+            var inputs = new Dictionary<string, object>();
+            var job = JobEngine.Start("workflowpack.empty", inputs);
+            var result = job.Task.Result;
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual("empty", result.Result);
+        }
+        public void ShouldSuccessWhenRunAdd100Workflow()
+        {
+            // var inputs = new Dictionary<string, object>()
+            // {
+            //     { "num" , 500 },
+            // };
+            // var job = JobEngine.Start("workflowpack.add100", inputs);
+            // var result = job.Task.Result;
+            // Assert.IsTrue(result.IsSuccess);
+            // Assert.AreEqual(600.0, result.Result);
+        }
+    }
+}
