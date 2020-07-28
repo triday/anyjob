23e5fb4f0b4d46d5c58320d2eb719e1139749cb1
pengbo.yang <obpt@sina.com>
2020-07-29 00:24:17 +0800

    add anyjob cli tool

---
+++ tool/AnyJob.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnyJob.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Anyjob!");
+        }
+    }
+}
