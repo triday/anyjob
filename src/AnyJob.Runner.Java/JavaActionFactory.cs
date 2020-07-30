35c2b225a4cf5d96f2373a999831f2858ca3a8b5
yangpengbo <obpt@sina.com>
2020-07-30 18:32:40 +0800

    add java warpper package

---
+++ src/AnyJob.Runner.Java/JavaActionFactory.cs
@@ -0,0 +1,12 @@
+using AnyJob;
+
+namespace src.AnyJob.Runner.Java
+{
+    public class JavaActionFactory : IActionFactoryService
+    {
+        public IAction CreateAction(IActionContext actionContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/AnyJob.Runner.Java/JavaOption.cs
+++ src/AnyJob.Runner.Java/JavaOption.cs
@@ -4,8 +4,8 @@
     public class JavaOption
     {
         public string JavaPath { get; set; } = "java";
-        public string WrapperPath { get; set; } = "global/java/java_wrapper.jar";
+        public string WrapperPath { get; set; } = "global/java/jar_libs/java_wrapper.jar";
         public string GlobalJarsPath { get; set; } = "global/java/jar_libs";
-        public string PackJarsPath { get; set; } = "jar_libs";
+        public string PackJarsPath { get; set; } = ".";
     }
 }
