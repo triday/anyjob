0d906c3ab3f5cd539785dfa33c55812fdb4df130
pengbo.yang <obpt@sina.com>
2020-08-09 20:56:37 +0800

    improve netcore unittest

---
+++ example/AnyJob.NetCore.Demo/InstanceWithArgument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnyJob.NetCore.Demo
+{
+    public class InstanceWithArgument
+    {
+        public InstanceWithArgument(string value)
+        {
+        }
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
--- example/AnyJob.NetCore.Demo/ValueTaskResult.cs
+++ example/AnyJob.NetCore.Demo/ValueTaskResult.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace AnyJob.NetCore.Demo
 {
-    public class TaskResult
+    public class ValueTaskResult
     {
-        public Task<int> Add(int num1, int num2)
+        public ValueTask<int> Add(int num1, int num2)
         {
-            return Task.FromResult(num1 + num2);
+            return new ValueTask<int>(num1 + num2);
         }
 
-        public Task<string> Concat(string a, string b)
+        public ValueTask<string> Concat(string a, string b)
         {
-            return Task.FromResult(a + b);
+            return new ValueTask<string>(a + b);
         }
-        public Task Hello(string name)
+        public ValueTask Hello(string name)
         {
             Console.WriteLine($"Hello,{name}");
-            return Task.CompletedTask;
+            return new ValueTask(Task.CompletedTask);
         }
 
-        public Task<List<PersonInfo>> Merge(List<PersonInfo> persons, PersonInfo other)
+        public ValueTask<List<PersonInfo>> Merge(List<PersonInfo> persons, PersonInfo other)
         {
             var result = new List<PersonInfo>();
 
@@ -32,7 +32,7 @@ namespace AnyJob.NetCore.Demo
             {
                 result.Add(other);
             }
-            return Task.FromResult(result);
+            return new ValueTask<List<PersonInfo>>(result);
         }
     }
 }
