763881c4f54bfcfd3b74e7f0e983ba5447995539
pengbo.yang <obpt@sina.com>
2020-08-09 20:26:18 +0800

    netcore unittest smoked

--- src/AnyJob.Runner.NetCore/NetCoreError.cs
+++ src/AnyJob.Runner.NetCore/NetCoreError.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Localization;
+using YS.Knife;
 
 namespace AnyJob.Runner.NetCore
 {
-    class NetCoreError
+    [ResourceClass]
+    public class NetCoreError
     {
+        private readonly IStringLocalizer stringLocalizer;
+
+        public NetCoreError(IStringLocalizer<NetCoreError> stringLocalizer)
+        {
+            this.stringLocalizer = stringLocalizer;
+        }
+
+        public ActionException InvalidEntryFormat(string entry)
+        {
+            return new ActionException();
+        }
     }
 }
