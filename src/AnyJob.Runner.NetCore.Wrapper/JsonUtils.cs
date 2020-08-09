6286e6753774ece8c4553a00fcaa06d1bb28cb8a
pengbo.yang <obpt@sina.com>
2020-08-09 13:39:09 +0800

    complete netcore wrapper

---
+++ src/AnyJob.Runner.NetCore.Wrapper/JsonUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace AnyJob.Runner.NetCore.Wrapper
+{
+    class JsonUtils
+    {
+        static JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+        {
+            IgnoreNullValues = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        public static JsonDocument FromFile(string filePath)
+        {
+            string all = File.ReadAllText(filePath);
+            return JsonDocument.Parse(all);
+        }
+        public static object Deserialize(string jsonText, Type type)
+        {
+            return JsonSerializer.Deserialize(jsonText, type, JsonSerializerOptions);
+        }
+        public static void WriteResultToFile(object result, string filePath)
+        {
+            string text = JsonSerializer.Serialize(result, JsonSerializerOptions);
+            File.WriteAllText(filePath, text);
+        }
+    }
+}
