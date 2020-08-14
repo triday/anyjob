faf43e61fcc23166260a9f7d8177c5bdabb59a16
pengbo.yang <obpt@sina.com>
2020-08-14 23:37:48 +0800

    add internal action info

---
+++ src/AnyJob.Engine/IInternalAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnyJob
+{
+    
+    /// <summary>
+    /// 表示内部的Action
+    /// </summary>
+    public interface IInternalAction:IAction
+    {
+    }
+}
